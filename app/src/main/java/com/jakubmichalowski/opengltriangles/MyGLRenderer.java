23232e71ba048983f8752709b47a4fde381089a1
jakub <user.name>
2017-10-29 13:26:42 +0100

    V 0.0.2 Działające figury, obracające się, kolorowe

---
+++ app/src/main/java/com/jakubmichalowski/opengltriangles/MyGLRenderer.java
@@ -0,0 +1,93 @@
+package com.jakubmichalowski.opengltriangles;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+/**
+ *  OpenGL Custom renderer used with GLSurfaceView 
+ */
+public class MyGLRenderer implements GLSurfaceView.Renderer {
+   Context context;   // Application's context
+
+   Triangle triangle;
+   Square square;
+
+   // Rotational angle and speed
+   private float angleTriangle = 0.0f;
+   private float angleQuad = 0.0f;
+   private float speedTriangle = 0.5f;
+   private float speedQuad = -0.4f;
+
+   // Constructor with global application context
+   public MyGLRenderer(Context context) {
+
+      this.context = context;
+
+      triangle = new Triangle();
+      square = new Square();
+   }
+   
+   // Call back when the surface is first created or re-created
+   @Override
+   public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+      gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);  // Set color's clear-value to black
+      gl.glClearDepthf(1.0f);            // Set depth's clear-value to farthest
+      gl.glEnable(GL10.GL_DEPTH_TEST);   // Enables depth-buffer for hidden surface removal
+      gl.glDepthFunc(GL10.GL_LEQUAL);    // The type of depth testing to do
+      gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);  // nice perspective view
+      gl.glShadeModel(GL10.GL_SMOOTH);   // Enable smooth shading of color
+      gl.glDisable(GL10.GL_DITHER);      // Disable dithering for better performance
+  
+      // You OpenGL|ES initialization code here
+      // ......
+   }
+   
+   // Call back after onSurfaceCreated() or whenever the window's size changes
+   @Override
+   public void onSurfaceChanged(GL10 gl, int width, int height) {
+      if (height == 0) height = 1;   // To prevent divide by zero
+      float aspect = (float)width / height;
+   
+      // Set the viewport (display area) to cover the entire window
+      gl.glViewport(0, 0, width, height);
+  
+      // Setup perspective projection, with aspect ratio matches viewport
+      gl.glMatrixMode(GL10.GL_PROJECTION); // Select projection matrix
+      gl.glLoadIdentity();                 // Reset projection matrix
+      // Use perspective projection
+      GLU.gluPerspective(gl, 45, aspect, 0.1f, 100.f);
+  
+      gl.glMatrixMode(GL10.GL_MODELVIEW);  // Select model-view matrix
+      gl.glLoadIdentity();                 // Reset
+  
+      // You OpenGL|ES display re-sizing code here
+      // ......
+   }
+   
+   // Call back to draw the current frame.
+   @Override
+   public void onDrawFrame(GL10 gl) {
+      // Clear color and depth buffers using clear-value set earlier
+      gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+     
+      // You OpenGL|ES rendering code here
+      gl.glLoadIdentity();    //Reset model-view matrix
+      gl.glTranslatef(-1.5f, 0.0f, -6.0f); // Translate left and into the screen
+      gl.glRotatef(angleTriangle, 0.0f, 1.0f, 0.0f); // Rotate the triangle about the y-axis
+      triangle.draw(gl);                   // Draw triangle
+
+      // Translate right, relative to the previous translation
+      gl.glLoadIdentity();                 // Reset the mode-view matrix
+      gl.glTranslatef(1.5f, 0.0f, -6.0f);  // Translate right and into the screen
+      gl.glRotatef(angleQuad, 1.0f, 0.0f, 0.0f); // Rotate the square about the x-axis
+      square.draw(gl);                       // Draw quad
+
+
+      // Update the rotational angle after each refresh
+      angleTriangle += speedTriangle;
+      angleQuad += speedQuad;
+   }
+}
---
+++ app/src/main/java/com/jakubmichalowski/opengltriangles/Triangle.java
@@ -0,0 +1,63 @@
+package com.jakubmichalowski.opengltriangles;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.opengles.GL10;
+/*
+ * A triangle with 3 vertices.
+ */
+public class Triangle {
+   private FloatBuffer vertexBuffer;  // Buffer for vertex-array
+   private FloatBuffer colorBuffer;   // Buffer for color-array
+   private ByteBuffer indexBuffer;    // Buffer for index-array
+  
+   private float[] vertices = {  // Vertices of the triangle
+       0.0f,  1.0f, 0.0f, // 0. top
+      -1.0f, -1.0f, 0.0f, // 1. left-bottom
+       1.0f, -1.0f, 0.0f  // 2. right-bottom
+   };
+   private byte[] indices = { 0, 1, 2 }; // Indices to above vertices (in CCW)
+
+   private float[] colors = { // Colors for the vertices (NEW)
+           1.0f, 0.0f, 0.0f, 1.0f, // Red (NEW)
+           0.0f, 1.0f, 0.0f, 1.0f, // Green (NEW)
+           0.0f, 0.0f, 1.0f, 1.0f  // Blue (NEW)
+   };
+
+   // Constructor - Setup the data-array buffers
+   public Triangle() {
+      // Setup vertex-array buffer. Vertices in float. A float has 4 bytes.
+      ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.length * 4);
+      vbb.order(ByteOrder.nativeOrder()); // Use native byte order
+      vertexBuffer = vbb.asFloatBuffer(); // Convert byte buffer to float
+      vertexBuffer.put(vertices);         // Copy data into buffer
+      vertexBuffer.position(0);           // Rewind
+
+      ByteBuffer cbb = ByteBuffer.allocateDirect(colors.length * 4);
+      cbb.order(ByteOrder.nativeOrder()); // Use native byte order
+      colorBuffer = cbb.asFloatBuffer();  // Convert byte buffer to float
+      colorBuffer.put(colors);            // Copy data into buffer
+      colorBuffer.position(0);            // Rewind
+
+      // Setup index-array buffer. Indices in byte.
+      indexBuffer = ByteBuffer.allocateDirect(indices.length);
+      indexBuffer.put(indices);
+      indexBuffer.position(0);
+   }
+  
+   // Render this shape
+   public void draw(GL10 gl) {
+      // Enable vertex-array and define the buffers
+      gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+      gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+      gl.glEnableClientState(GL10.GL_COLOR_ARRAY);          // Enable color-array
+      gl.glColorPointer(4, GL10.GL_FLOAT, 0, colorBuffer);  // Define color-array buffer
+
+      // Draw the primitives via index-array
+      gl.glDrawElements(GL10.GL_TRIANGLES, indices.length, GL10.GL_UNSIGNED_BYTE, indexBuffer);
+      gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+      gl.glDisableClientState(GL10.GL_COLOR_ARRAY);   // Disable color-array
+   }
+}
